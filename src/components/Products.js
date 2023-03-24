decb8ad8f1ccaebadc90547380e152d6c7f2f99c
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-03-25 05:48:22 +0600

    Complete Assignment

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,20 @@
-/* eslint-disable react/prop-types */
+// /* eslint-disable react/prop-types */
+
+import React from 'react';
+import Product from './Product';
+
+
+function Products (props) {
+    
+    const {products} = props;
+
+    return (
+        <div className='products'>
+            {products.map((product) => (
+                <Product key={product.id} product={product}/>
+            ))}
+        </div>
+    )
+}
+
+export default Products;
